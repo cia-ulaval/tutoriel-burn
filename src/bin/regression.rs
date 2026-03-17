f0357219f384b8040405dd1f4aae68a766a4abb8
Cielbird <guilhem.ane3@gmail.com>
2026-03-16 22:17:27 -0400

    create crate

---
+++ src/bin/regression.rs
@@ -0,0 +1,60 @@
+#![recursion_limit = "256"]
+
+use burn::{backend::Autodiff, tensor::backend::Backend};
+use simple_regression::{inference, training};
+
+static ARTIFACT_DIR: &str = "/tmp/burn-example-regression";
+
+#[cfg(any(
+    feature = "ndarray",
+    feature = "ndarray-blas-netlib",
+    feature = "ndarray-blas-openblas",
+    feature = "ndarray-blas-accelerate",
+))]
+mod ndarray {
+    use burn::backend::ndarray::{NdArray, NdArrayDevice};
+
+    pub fn run() {
+        let device = NdArrayDevice::Cpu;
+        super::run::<NdArray>(device.clone());
+    }
+}
+
+#[cfg(feature = "cuda")]
+mod cuda {
+    use burn::backend::{Autodiff, Cuda};
+
+    pub fn run() {
+        let device = Default::default();
+        super::run::<Cuda>(device);
+    }
+}
+
+#[cfg(any(feature = "wgpu", feature = "metal"))]
+mod wgpu {
+    use burn::backend::wgpu::{Wgpu, WgpuDevice};
+
+    pub fn run() {
+        let device = WgpuDevice::default();
+        super::run::<Wgpu>(device);
+    }
+}
+
+/// Train a regression model and predict results on a number of samples.
+pub fn run<B: Backend>(device: B::Device) {
+    training::run::<Autodiff<B>>(ARTIFACT_DIR, device.clone());
+    inference::infer::<B>(ARTIFACT_DIR, device)
+}
+
+fn main() {
+    #[cfg(any(
+        feature = "ndarray",
+        feature = "ndarray-blas-netlib",
+        feature = "ndarray-blas-openblas",
+        feature = "ndarray-blas-accelerate",
+    ))]
+    ndarray::run();
+
+    #[cfg(any(feature = "wgpu", feature = "metal"))]
+    wgpu::run();
+}
